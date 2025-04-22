8a84387ac9f3328972bef585f73039399c3c261e
Muhammed althaf <althafpilachalil132@gmail.com>
2025-04-23 04:09:00 +0530

    Google authentication is changed

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const env = require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback" // Updated here
+    callbackURL: process.env.GOOGLE_CALLBACK_URL 
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
